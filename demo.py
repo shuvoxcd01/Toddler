c2bd1c54c7e67a03307a87383f322660bf775d63
Falguni Das <dasshuvo7@gmail.com>
2020-08-15 16:16:16 +0600

    Add demo.py

---
+++ demo.py
@@ -0,0 +1,36 @@
+import chess
+import chess.svg
+from state import ChessState
+from mcts import uct
+
+players = ["Black", "White"]
+
+
+def play_game():
+    """
+    Play a sample game between two UCT players where each player gets a different number of UCT iterations.
+    """
+    board = chess.Board()
+    board.reset()
+    print(chess.svg.board(board))
+    state = ChessState(board=board, side_to_move=board.turn)
+
+    while state.get_moves():
+        print(str(state))
+        if state.player_just_moved == chess.BLACK:
+            m = uct(root_state=state, max_iteration=10, verbose=False)  # White
+        else:
+            m = uct(root_state=state, max_iteration=1, verbose=False)  # Black
+
+        print("Best Move: " + str(m) + "\n")
+        state.do_move(m)
+
+    if state.get_result(state.player_just_moved) == 1.0:
+        print("Player " + players[int(state.player_just_moved)] + " wins!")
+    elif state.get_result(state.player_just_moved) == 0.0:
+        print("Player " + players[int(not state.player_just_moved)] + "wins!")
+    else:
+        print("Nobody wins!")
+
+
+play_game()
